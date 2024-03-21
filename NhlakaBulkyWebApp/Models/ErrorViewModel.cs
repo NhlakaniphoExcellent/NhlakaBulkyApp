94294c6cfbe015a47e39980c38b654460664faa0
Nhlakanipho Excellent Mthombeni <22214349@dut4life.ac.za>
2024-03-21 19:22:48 +0200

    Add project files.

---
+++ NhlakaBulkyWebApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace NhlakaBulkyWebApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
