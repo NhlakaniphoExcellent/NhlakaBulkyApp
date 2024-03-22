cafba06f5cec9c085d017532686041240b0ed305
Nhlakanipho Excellent Mthombeni <22214349@dut4life.ac.za>
2024-03-22 17:52:58 +0200

    Adding CRUD to Category and add bootstrap functionality

--- NhlakaBulkyWebApp/Controllers/CategoryController.cs
+++ NhlakaBulkyWebApp/Controllers/CategoryController.cs
@@ -20,9 +20,116 @@ namespace NhlakaBulkyWebApp.Controllers
             return View(categoryObjectList);
         }
 
+        [HttpGet]
         public IActionResult Create()
         {
             return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Category category)
+        {
+            if (category.Name == category.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("", "The Display Order cannot be the same as Catergory Name");
+            }
+
+            /*   if (category.Name == category.DisplayOrder.ToString())
+               {
+                   ModelState.AddModelError("", "The Display Order cannot be the same as Catergory Name");
+               } */
+
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Add(category);
+                _db.SaveChanges();
+                TempData["Created"] = "Category Created Successfully";
+                return RedirectToAction("Index");
+            }
+
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Edit(int? Id)
+        {
+            if(Id == null || Id == 0)
+            {
+                return NotFound();
+            }
+            Category? categoryFromDB = _db.Categories.FirstOrDefault(x => x.ID == Id);
+
+            if(categoryFromDB == null)
+            {
+                return NotFound();
+            }
+
+            return View(categoryFromDB);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(Category category)
+        {
+            //if (category.Name == category.DisplayOrder.ToString())
+            //{
+            //    ModelState.AddModelError("", "The Display Order cannot be the same as Catergory Name");
+            //}
+            /*   if (category.Name == category.DisplayOrder.ToString())
+               {
+                   ModelState.AddModelError("", "The Display Order cannot be the same as Catergory Name");
+               } */
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Update(category);
+                _db.SaveChanges();
+                TempData["Created"] = "Category Updated Successfully";
+                return RedirectToAction("Index");
+            }
+
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Delete(int? Id)
+        {
+            if (Id == null || Id == 0)
+            {
+                return NotFound();
+            }
+            Category? categoryFromDB = _db.Categories.FirstOrDefault(x => x.ID == Id);
+
+            if (categoryFromDB == null)
+            {
+                return NotFound();
+            }
+
+            return View(categoryFromDB);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeletePost(int? Id)
+        {
+            //if (category.Name == category.DisplayOrder.ToString())
+            //{
+            //    ModelState.AddModelError("", "The Display Order cannot be the same as Catergory Name");
+            //}
+            /*   if (category.Name == category.DisplayOrder.ToString())
+               {
+                   ModelState.AddModelError("", "The Display Order cannot be the same as Catergory Name");
+               } */
+
+            Category? categoryFromDB = _db.Categories.FirstOrDefault(x => x.ID == Id);
+            if (categoryFromDB == null)
+            {
+                return NotFound();
+            }
+            _db.Categories.Remove(categoryFromDB);
+            _db.SaveChanges();
+            TempData["Created"] = "Category Deleted Successfully";
+            return RedirectToAction("Index");
         }
     }
 }
--- NhlakaBulkyWebApp/Models/Category.cs
+++ NhlakaBulkyWebApp/Models/Category.cs
@@ -7,10 +7,15 @@ namespace NhlakaBulkyWebApp.Models
     {
         [Key]
         public int ID { get; set; }
+
         [Required]
         [DisplayName("Category Name")]
+        [MaxLength(40)]
         public string Name { get; set; }
+
+        [Required]
         [DisplayName("Display Order")]
+        [Range(1, 10000, ErrorMessage = "Dispaly Order needs to be between 1 & 10 000")]
         public int DisplayOrder { get; set; }
     }
 }
